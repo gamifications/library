a01f5c7bef30bcb35f9f7de674ffb794f7c018f1
suhailvs <suhailvs@gmail.com>
2021-06-01 09:38:21 +0530

    a small fix

--- library/urls.py
+++ library/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name='library'
 
 urlpatterns = [
-    path('books/', views.BookListView.as_view(), name='book-list'),
+    path('', views.BookListView.as_view(), name='book-list'),
     path('<int:pk>/borrow/', views.check_out, name='check_out'),
     path('<int:pk>/return/', views.check_in, name='check_in'),
     path('my_books/', views.my_books, name='my-books'),
